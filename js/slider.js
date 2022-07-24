8361df04be2b468ff7fcb48891ee5e8a73d9f628
Kyle Bacon <hello@kylebaconcreative.com>
2022-07-24 08:54:10 -0700

    Update slider.js

--- js/slider.js
+++ js/slider.js
@@ -22,23 +22,18 @@
     };
     
     const getMousePos = (e) => {
-        let rect = e.target.getBoundingClientRect();
-        let x = e.clientX - rect.left;
-        let y = e.clientY - rect.top;
-        return { x : x, y : y }
-        
-//         let posx = 0;
-//         let posy = 0;
-//         if (!e) e = window.event;
-//         if (e.pageX || e.pageY) {
-//             posx = e.pageX;
-//             posy = e.pageY;
-//         }
-//         else if (e.clientX || e.clientY) 	{
-//             posx = e.clientX + body.scrollLeft + document.documentElement.scrollLeft;
-//             posy = e.clientY + body.scrollTop + document.documentElement.scrollTop;
-//         }
-//         return { x : posx, y : posy }
+        let posx = 0;
+        let posy = 0;
+        if (!e) e = window.event;
+        if (e.pageX || e.pageY) {
+            posx = e.pageX;
+            posy = e.pageY;
+        }
+        else if (e.clientX || e.clientY) 	{
+            posx = e.clientX + body.scrollLeft + document.documentElement.scrollLeft;
+            posy = e.clientY + body.scrollTop + document.documentElement.scrollTop;
+        }
+        return { x : posx, y : posy }
     };
 
     let winsize;
